6e1b077d81c50d43b0ddb4eccc6e231dbc446dab
Lucas Dimitroff <ldimitroff@devspark.com>
2015-05-21 17:28:25 -0300

    Adding item click to edit ToDo

--- DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/MainActivity.java
+++ DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,26 +19,39 @@ public class MainActivity extends ActionBarActivity {
 
 	private static final int NEW_EDIT_TODO = 1;
 	public static final String NEW_TODO_RESULT_FIELD = "result";
-	private List<String> toDoList;
-    private ToDoAdapter toDoAdapter;
-    private ListView toDoListView;
+	public static final String TODO_TEXT = "todotext";
+	public static final String TODO_POSITION = "todoposition";
 
-    @Override
+	private List<String> toDoList;
+	private ToDoAdapter toDoAdapter;
+	private ListView toDoListView;
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-        bindViews();
+		bindViews();
 		toDoList = new ArrayList<String>();
-        toDoAdapter = new ToDoAdapter(this, toDoList);
+		toDoAdapter = new ToDoAdapter(this, toDoList);
 
-        toDoListView.setAdapter(toDoAdapter);
+		toDoListView.setAdapter(toDoAdapter);
 	}
 
-    private void bindViews() {
-        toDoListView = (ListView)findViewById(R.id.toDolistView);
-    }
+	private void bindViews() {
+		toDoListView = (ListView)findViewById(R.id.toDolistView);
 
-    @Override
+		toDoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(MainActivity.this, NewEditToDo.class);
+				intent.putExtra(TODO_TEXT, toDoList.get(position));
+				intent.putExtra(TODO_POSITION, position);
+				startActivityForResult(intent, NEW_EDIT_TODO);
+			}
+		});
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -63,9 +78,14 @@ public class MainActivity extends ActionBarActivity {
 		if (requestCode == NEW_EDIT_TODO) {
 			if (resultCode == RESULT_OK) {
 				String result = data.getStringExtra(NEW_TODO_RESULT_FIELD);
+				int position = data.getIntExtra(TODO_POSITION, -1);
 				if (result != null && !result.isEmpty()) {
-					toDoList.add(result);
-                    toDoAdapter.notifyDataSetChanged();
+					if (position >= 0) {
+                        toDoList.set(position, result);
+					} else {
+						toDoList.add(result);
+					}
+					toDoAdapter.notifyDataSetChanged();
 				} else {
 					Toast.makeText(this, "Empty Input", Toast.LENGTH_SHORT).show();
 				}
--- DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/NewEditToDo.java
+++ DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/NewEditToDo.java
@@ -11,23 +11,33 @@ public class NewEditToDo extends ActionBarActivity {
 
 	private Button addToDoButton;
 	private EditText toDoEditText;
+	private String toDoText;
+	private int toDoPosition;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_edit_to_do);
 
+		Intent i = getIntent();
+		toDoText = i.getStringExtra(MainActivity.TODO_TEXT);
+		toDoPosition = i.getIntExtra(MainActivity.TODO_POSITION, -1);
 		bindViews();
 	}
 
 	private void bindViews() {
 		toDoEditText = (EditText)findViewById(R.id.toDoEditText);
-		addToDoButton = (Button)findViewById(R.id.addToDoButton);
 
+		if (toDoText != null && !toDoText.isEmpty()) {
+			toDoEditText.setText(toDoText);
+		}
+
+		addToDoButton = (Button)findViewById(R.id.addToDoButton);
 		addToDoButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent returnIntent = new Intent();
+                returnIntent.putExtra(MainActivity.TODO_POSITION, toDoPosition);
 				returnIntent.putExtra(MainActivity.NEW_TODO_RESULT_FIELD, toDoEditText.getText().toString());
 				setResult(RESULT_OK, returnIntent);
 				finish();
