875471f3c2ba9d4557868f2c384d1b864c2d6c50
Lucas Dimitroff <ldimitroff@devspark.com>
2015-05-21 17:13:56 -0300

    Adding a List View and the Adapter

--- DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/MainActivity.java
+++ DsTrainingToDoApp/app/src/main/java/training/devspark/dstodotrainingapp/MainActivity.java
@@ -5,20 +5,38 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import training.devspark.dstodotrainingapp.adapter.ToDoAdapter;
 
 public class MainActivity extends ActionBarActivity {
 
 	private static final int NEW_EDIT_TODO = 1;
-    public static final String NEW_TODO_RESULT_FIELD = "result";
+	public static final String NEW_TODO_RESULT_FIELD = "result";
+	private List<String> toDoList;
+    private ToDoAdapter toDoAdapter;
+    private ListView toDoListView;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        bindViews();
+		toDoList = new ArrayList<String>();
+        toDoAdapter = new ToDoAdapter(this, toDoList);
+
+        toDoListView.setAdapter(toDoAdapter);
 	}
 
-	@Override
+    private void bindViews() {
+        toDoListView = (ListView)findViewById(R.id.toDolistView);
+    }
+
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -45,7 +63,12 @@ public class MainActivity extends ActionBarActivity {
 		if (requestCode == NEW_EDIT_TODO) {
 			if (resultCode == RESULT_OK) {
 				String result = data.getStringExtra(NEW_TODO_RESULT_FIELD);
-				Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+				if (result != null && !result.isEmpty()) {
+					toDoList.add(result);
+                    toDoAdapter.notifyDataSetChanged();
+				} else {
+					Toast.makeText(this, "Empty Input", Toast.LENGTH_SHORT).show();
+				}
 			}
 			if (resultCode == RESULT_CANCELED) {
 				Toast.makeText(this, "User Cancelled", Toast.LENGTH_SHORT).show();
